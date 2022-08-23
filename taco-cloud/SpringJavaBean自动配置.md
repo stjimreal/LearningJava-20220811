d4b06b918835c69006e3c9fa56484d1ec5d0152f
liuljing <liuljing@yonyou.com>
2022-08-23 14:58:04 +0800

    Repair the error of circular reference of DataSource when Scanning beans.

---
+++ taco-cloud/SpringJavaBean自动配置.md
@@ -0,0 +1,19 @@
+# Spring Boot的自动配置
+
+## AutoConfiguration和 Configuration 的区别是什么？
+
+1. AutoConfiguration 和 Configuration 的区别是什么？
+2. 为什么 SpringBootApplication 的 exclude 不能作用于@AutoConfiguration类？
+3. 为什么标记为 @AutoConfiguration 的类自动解除了DataSource Bean的循环依赖？而标记为 Configuration 却无法做到？
+
+## SpringBootApplication
+
+==> SpringBootConfiguration + EnableAutoConfiguration + ComponentScan
+
+## 由两个 DataSource Bean 造成的循环依赖是什么？是如何解除的？
+
+循环依赖？
+
+1. 在其中一个不使用Spring Boot自动配置的 main 启动类配置文件 .xml 中限定组件扫描位置，区分DataSource位置
+2. 在另一个配置了@SpringBootApplication的main启动类中装配另一个类时，标记对方为 @AutoConfiguration
+
--- taco-cloud/src/main/java/com/exampleLLJ/tacocloud/DataSourcePreviewApplication.java
+++ taco-cloud/src/main/java/com/exampleLLJ/tacocloud/DataSourcePreviewApplication.java
@@ -1,29 +1,50 @@
-package com.example.LearnException;
+package com.exampleLLJ.tacocloud;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.SQLException;
 
+/**
+ * @program: Learning-20220811
+ * @description:
+ * @author: liuljing
+ * @created: 2022/08/18 19:48
+ */
 @SpringBootApplication
 @Slf4j
-public class LearnExceptionApplication implements CommandLineRunner {
-
+public class DataSourcePreviewApplication implements CommandLineRunner {
     @Resource
     DataSource dataSource;
+
+    @Resource
+    JdbcTemplate jdbcTemplate;
+
     public static void main(String[] args) {
-        SpringApplication.run(LearnExceptionApplication.class, args);
+        SpringApplication.run(DataSourcePreviewApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
+        showConnection();
+        showData();
+    }
+
+    private void showConnection() throws SQLException {
         log.info("The DataSource is " + dataSource.toString());
         Connection conn = dataSource.getConnection();
         log.info("Your Connection is " + conn.toString());
         conn.close();
+    }
+
+    private void showData() {
+        jdbcTemplate.queryForList("SELECT * FROM FOO")
+                .forEach(row -> log.info(row.toString()));
     }
 }
